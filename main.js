0cdbe011fbd56269eebce0506250ef5ce6bff98f
Joshua Manley <verymanley@protonmail.com>
2020-06-04 09:04:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ window.addEventListener("scroll", function()
   prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.2 + "px";
   })
 })
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("slide_up").className = "slideUp";
+  }
+}
